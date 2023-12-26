734b029baef05396373217b7104beb2b6e524a28
joeuni-ex <141595215+joeuni-ex@users.noreply.github.com>
2023-12-26 15:29:49 +0900

    링크로 페이지 이동

--- src/layout/Navbar.jsx
+++ src/layout/Navbar.jsx
@@ -1,13 +1,14 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
     <>
       <nav className="navbar navbar-expand-md bg-primary navbar-dark">
         <div className="container-fluid">
-          <a className="navbar-brand text-warning" href="#">
+          <Link to="/" className="navbar-brand text-warning">
             리액트 & 스프링부트
-          </a>
+          </Link>
           <button
             className="navbar-toggler"
             type="button"
@@ -22,7 +23,9 @@ const Navbar = () => {
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
               <li className="nav-item">
-                <button className="btn btn-outline-light">유저 추가</button>
+                <Link to="/adduser" className="btn btn-outline-light">
+                  유저 추가
+                </Link>
               </li>
             </ul>
           </div>
