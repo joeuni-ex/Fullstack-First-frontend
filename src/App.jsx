65b614c19611de9f5aab3c9d254626d0597d6186
joeuni-ex <141595215+joeuni-ex@users.noreply.github.com>
2023-12-26 14:38:05 +0900

    홈페이지 생성 및 컴포넌트 적용

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import "./App.css";
 import Navbar from "./layout/Navbar";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.js";
+import Home from "./pages/Home";
 
 function App() {
   return (
     <>
       <div className="App">
         <Navbar />
+        <Home />
       </div>
     </>
   );
