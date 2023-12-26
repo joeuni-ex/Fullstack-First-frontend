fe942295dca494a0caf14c4815350743b6c3e930
joeuni-ex <141595215+joeuni-ex@users.noreply.github.com>
2023-12-26 15:26:39 +0900

    리액트 라우터 적용

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,19 @@ import Navbar from "./layout/Navbar";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.js";
 import Home from "./pages/Home";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import AddUser from "./users/AddUser";
 
 function App() {
   return (
     <>
-      <div className="App">
+      <BrowserRouter>
         <Navbar />
-        <Home />
-      </div>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="adduser" element={<AddUser />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
