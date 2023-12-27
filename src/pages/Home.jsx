5ffd8e83d1911a4ab2204d81860b752a456ce1aa
joeuni <dmsdl8917@gmail.com>
2023-12-27 23:32:39 +0900

    검색 기능 추가

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,6 +4,8 @@ import { Link } from "react-router-dom";
 
 const Home = () => {
   const [users, setUsers] = useState([]);
+  const [search, setSearch] = useState("");
+  const [searchResult, setSearchResult] = useState([]);
 
   useEffect(() => {
     getUsers();
@@ -22,8 +24,45 @@ const Home = () => {
       getUsers(); //삭제 후 유저 정보 다시 가져오기
     }
   };
+  //검색
+  const searchUser = async (e) => {
+    e.preventDefault();
+    if (search === "") {
+      getUsers();
+      return;
+    }
+    const result = await axios.get(
+      `${import.meta.env.VITE_USER_API}/searchUser?search=${search}`
+    );
+    setUsers(result.data);
+  };
+
+  const onChange = (e) => {
+    setSearch(e.target.value);
+  };
   return (
     <div className="container">
+      <form onSubmit={searchUser}>
+        <div className="d-flex justify-content-center ">
+          <div className="input-group mb-3  w-50 p-3">
+            <input
+              onChange={onChange}
+              type="text"
+              className="form-control mt-4 "
+              placeholder="Recipient's username"
+              aria-label="Recipient's username"
+              aria-describedby="button-addon2"
+            />
+            <button
+              className="btn btn-outline-secondary  mt-4"
+              type="submit"
+              id="button-addon2"
+            >
+              Button
+            </button>
+          </div>
+        </div>
+      </form>
       <table className="table  border shadow text-center  my-4 table-hover">
         <thead>
           <tr>
